23b0dc9e69e84083873d9cffea4a8506badb880a
Yai <myd.rio@hotmail.com>
2021-05-08 15:17:28 +0200

    correct order when multiple of 100

--- pages/api/picsum.api.js
+++ pages/api/picsum.api.js
@@ -9,7 +9,22 @@ export const getPictures = async (page = 1, limit = 100) => {
 		if (page === 1) {
 			cleanImages.push({ author: pix.author, order: idx + 1 < 10 ? `#0${idx + 1}` : `#${idx + 1}`, url: `https://picsum.photos/id/${pix.id}/400` })
 		} else {
-			cleanImages.push({ author: pix.author, order: idx + 1 < 10 ? `#${page - 1}0${idx + 1}` : `#${page - 1}${idx + 1}`, url: `https://picsum.photos/id/${pix.id}/400` })
+			let order = ''
+			if ((idx + 1) % 100 == 0) {
+				order = `#${page}00`
+			} else {
+				if (idx + 1 < 10) {
+					order = `#${page - 1}0${idx + 1}`
+				} else {
+					order = `#${page - 1}${idx + 1}`
+				}
+			}
+			let auxImage = {
+				author: pix.author,
+				order: order,
+				url: `https://picsum.photos/id/${pix.id}/400`,
+			}
+			cleanImages.push(auxImage)
 		}
 	})
 
